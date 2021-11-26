3ae04d34eca00a01879691d8c107d0ded40a07c6
ratan othayoth <ratanpub@protonmail.com>
2021-11-25 23:49:09 -0500

    reasonably working with reference tracking

--- teeterbot_gazebo/src/robot_controller.py
+++ teeterbot_gazebo/src/robot_controller.py
@@ -4,6 +4,7 @@ import math
 import tf
 from geometry_msgs.msg import Pose
 from geometry_msgs.msg import Quaternion
+from geometry_msgs.msg import Twist
 from std_msgs.msg import Float64
 from std_msgs.msg import Bool
 from gazebo_msgs.msg import ModelStates
@@ -19,38 +20,32 @@ class robot_controller():
         self.on = True
         self.upright = True
 
-        self.x = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0] #state
+        self.x = [0.0, 0.0, 0.0, 0.0] # state = [forward velocity, body tilt, body tilt velocity, turning velocity]
+        
+        self.ref_forward_speed = 0.0  # reference forward speed specified by the user        
+        self.ref_turning_speed = 0.0  # reference turning speed specified by the user
 
-        self.E = 0.0        # proportional error
-        self.Eold = 0.0     # previous error
-        self.Eint = 0.0     # integral error
-        self.Edot = 0.0     # derivative error
-        self.Kp = 0.0     # proportional gain
-        self.Ki = 0.0     # integral gain
-        self.Kd = 0.0      # derivative gain
 
-        self.K_lqr_1 = [ -2.2361,-20.6729,-3.8664, 2.23611]
-        self.K_lqr_2 = [ -2.2361,-20.6729,-3.8664,-2.23611]
+        # State feedback gains from LQR
+        self.K_lqr_right = [ -2.2361,-19.3436,-2.8267, 2.23611]
+        self.K_lqr_left  = [ -2.2361,-19.3436,-2.8267,-2.23611]
 
+        self.g_lqr_right = [ -2.2361, 2.2361]
+        self.g_lqr_left  = [ -2.2361,-2.2361]
+
+        # control torques for right and left wheels
         self.control_effort = [0.0,0.0]
-        self.TLim = 8.0
-
-        self.euler= [0.0,0.0,0.0]
+        
+        # body orientations
+        self.euler = [0.0,0.0,0.0]
         self.roll  = 0.0
         self.pitch = 0.0
         self.yaw   = 0.0
-
-        self.right_wheel_omega = 0.0
-        self.left_wheel_omega = 0.0
-        self.right_wheel_vx = 0.0
-        self.left_wheel_vx = 0.0
-        self.chassis_vx = 0.0
-
-
-
+        
         # create publishers and subscribers
-        sub = rospy.Subscriber("gazebo/model_states", ModelStates, self.pid_controller)
-        sub_fallen = rospy.Subscriber("teeterbot/fallen_over", Bool, self.reset_controller)
+        sub_model       = rospy.Subscriber("gazebo/model_states", ModelStates, self.state_feedback_controller)
+        sub_reference   = rospy.Subscriber("/cmd_vel", Twist, self.reference_callback)
+        sub_fallen      = rospy.Subscriber("teeterbot/fallen_over", Bool, self.reset_controller)
         pub_right_wheel = rospy.Publisher("teeterbot/right_torque_cmd", Float64, queue_size=10)
         pub_left_wheel  = rospy.Publisher("teeterbot/left_torque_cmd", Float64, queue_size=10)
 
@@ -66,8 +61,9 @@ class robot_controller():
                     pub_left_wheel.publish(0.0)
                 self.got_new_msg = False
 
-    def reset_controller(self,msg):
+    def reset_controller(self,msg):        
         if(msg.data):
+            print("teeterbot fell over")
             self.Eold = 0
             self.E = 0
             self.control_effort = [0.0,0.0]
@@ -76,70 +72,50 @@ class robot_controller():
         else:
             self.upright = True
             self.got_new_msg  = True
+    
+    # update references as the commands come in
+    def reference_callback(self,msg):
+        self.ref_forward_speed = msg.linear.x
+        self.ref_turning_speed = msg.angular.z
 
-    def pid_controller(self,msg):
-
+    def state_feedback_controller(self,msg):
         
         # convert orientation to roll pitch yaw
         quat = (msg.pose[1].orientation.x,
                 msg.pose[1].orientation.y,
                 msg.pose[1].orientation.z,
                 msg.pose[1].orientation.w)
-
         self.euler = tf.transformations.euler_from_quaternion(quat)
-        self.roll = self.euler[0]
+        self.roll  = self.euler[0]
         self.pitch = self.euler[1]
-        self.yaw = self.euler[2]
+        self.yaw   = self.euler[2]
     
-        # observed the states
-        self.x[0] = msg.twist[1].linear.x
-        self.x[1] = self.pitch
-        self.x[2] = msg.twist[1].angular.y
-        self.x[3] = msg.twist[1].angular.z
+        # observed states
+        self.x[0] = msg.twist[1].linear.x       # forward velocity
+        self.x[1] = self.pitch                  # body tilt
+        self.x[2] = msg.twist[1].angular.y      # body tilt velocity
+        self.x[3] = msg.twist[1].angular.z      # turning velocity
 
-        self.control_effort[0] = -(self.K_lqr_1[0]*self.x[0] + 
-                                  self.K_lqr_1[1]*self.x[1] +
-                                  self.K_lqr_1[2]*self.x[2] +
-                                  self.K_lqr_1[3]*self.x[3])
-        self.control_effort[1] = -(self.K_lqr_2[0]*self.x[0] + 
-                                  self.K_lqr_2[1]*self.x[1] +
-                                  self.K_lqr_2[2]*self.x[2] +
-                                  self.K_lqr_2[3]*self.x[3])                                  
+        # control effort u = -K*x for state feedback controller
+        self.control_effort[0] = -(self.K_lqr_right[0]*self.x[0] + 
+                                   self.K_lqr_right[1]*self.x[1] +
+                                   self.K_lqr_right[2]*self.x[2] +
+                                   self.K_lqr_right[3]*self.x[3])
+        self.control_effort[0] = self.control_effort[0] + self.g_lqr_right[0]*self.ref_forward_speed + self.g_lqr_right[1]*self.ref_turning_speed
+        self.control_effort[1] = -(self.K_lqr_left[0]*self.x[0] + 
+                                   self.K_lqr_left[1]*self.x[1] +
+                                   self.K_lqr_left[2]*self.x[2] +
+                                   self.K_lqr_left[3]*self.x[3])                                  
+        self.control_effort[1] = self.control_effort[1] + self.g_lqr_left[0]*self.ref_forward_speed + self.g_lqr_left[1]*self.ref_turning_speed                                   
 
-        # # calculate error
-        # self.E = self.pitch
-
-        # # calculate integral error
-        # self.Eint = self.Eint + self.E
-
-        # # calculate derivative error
-        # self.Edot = self.E  - self.Eold
-
-        # # update previous error
-        # self.Eold = self.E
-
-        # # calculate control effort
-        # control = self.Kp*self.E + self.Ki*self.Eint + self.Kd*self.Edot
-        # self.control_effort[0] = math.copysign(1,control)*min(abs(control),self.TLim)
-        # self.control_effort[1] = math.copysign(1,control)*min(abs(control),self.TLim)
-
-        # #cutoff control within 5 degree                
-        # if -5.0 < (self.pitch*180.0/math.pi) < 5.0:
-        #     self.control_effort[0] = 0.0
-        #     self.control_effort[1] = 0.0
-        # # cutoff control beyond 45 degree                
-        # if  (self.pitch*180.0/math.pi) < -45.0 or (self.pitch*180.0/math.pi) > 45.0:
-        #     self.control_effort[0] = 0.0
-        #     self.control_effort[1] = 0.0
-
-        #cutoff if rolls too much
-        if abs(self.roll*180/math.pi) > 5 or abs(self.pitch*180/math.pi)>55:
+        # cutoff control if robot rolls or pitches too much
+        if abs(self.roll*180/math.pi) > 5 or abs(self.pitch*180/math.pi)>75:
             self.control_effort=[0.0, 0.0]
 
-        #print(self.E)
+        
         print("Torques: " + str(self.control_effort[0]) + ", " + str(self.control_effort[1]))
 
-        #self.control_effort = [0.0,0.0]
+        
         self.got_new_msg  = True
 
     def config_callback(self,config,level):
